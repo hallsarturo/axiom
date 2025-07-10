e1eac39194563f801c60b99076ea336819d53041
hallsarturo <proalarturo@gmail.com>
2025-07-10 13:08:48 -0600

    feat: add buttons for Sign Up and Learn more, update dependencies

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 'use client';
 
-import Image from 'next/image';
 import { useState, useEffect } from 'react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link'
+
 const wordList = ['rage', 'stolen data', 'fake news'];
 
 export default function Home() {
@@ -19,12 +21,14 @@ export default function Home() {
             {/* Navigation/menu bar is rendered by layout.js above this */}
             <main className="flex-1 flex flex-col justify-center items-center bg-gradient-to-br from-blue-600 to-purple-700 text-white px-6 text-center">
                 <div className="max-w-4xl">
-                    <h2 className="text-6xl">
-                        No more{' '}
-                        <span className="inline-block align-baseline relative w-[12ch] h-[3.5rem] overflow-hidden">
-                            <div 
-                            className="absolute top-0 left-0 transition-transform duration-700"
-                            style={{ transform: `translateY(-${index * 3.6}rem)` }}
+                    <h2 className="text-6xl flex justify-center items-center">
+                        No more{'  '}
+                        <span className="inline-block align-baseline relative w-1/2 mx-auto h-[3.5rem] overflow-hidden ml-8">
+                            <div
+                                className="absolute top-0 left-0 transition-transform duration-700"
+                                style={{
+                                    transform: `translateY(-${index * 3.6}rem)`,
+                                }}
                             >
                                 {wordList.map((word, i) => (
                                     <div
@@ -43,6 +47,12 @@ export default function Home() {
                     <h3 className="text-4xl leading-relaxed drop-shadow-md">
                         Knowledge first social network for serious intellectuals
                     </h3>
+                    <div className="mt-8">
+                        <Button className=" mr-10">Sign Up</Button>
+                        <Button className="bg-transparent" variant="outline">
+                            <Link href="/">Learn more</Link>
+                        </Button>
+                    </div>
                 </div>
             </main>
         </div>
