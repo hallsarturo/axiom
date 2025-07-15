efa882c6a131dfed7146cf1aa91b5f53ae19b180
hallsarturo <proalarturo@gmail.com>
2025-07-15 11:51:09 -0600

    feat: update redirect path to user dashboard after successful signup and add dashboard component

---
+++ app/(connected)/dashboard/page.js
@@ -0,0 +1,10 @@
+export default function Feed() {
+    return (
+        <div className="flex min-h-screen justify-center items-center mt-6">
+            <main>
+                <h1 className="text-5xl">Dahsboard</h1>
+                <h2>Wellcome user!</h2>
+            </main>
+        </div>
+    );
+}
