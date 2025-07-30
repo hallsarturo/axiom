1e40469368215d5508f321050969ffbbda904c33
hallsarturo <proalarturo@gmail.com>
2025-07-29 19:05:08 -0600

    feat: update Profile component layout and styling; integrate user data and enhance visual presentation

--- app/(connected)/profile/page.js
+++ app/(connected)/profile/page.js
@@ -1,128 +1,112 @@
 'use client';
 
 import { PaperClipIcon } from '@heroicons/react/20/solid';
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { useUser } from '@/components/context/UserProfileContext';
 
 export default function Profile() {
+    const { user } = useUser();
     return (
-        <div className="mx-auto my-12 max-w-4xl">
-            <div className="px-4 sm:px-0">
-                <h3 className="text-base/7 font-semibold text-white">
-                    Applicant Information
-                </h3>
-                <p className="mt-1 max-w-2xl text-sm/6 text-gray-400">
-                    Personal details and application.
-                </p>
-            </div>
-            <div className="mt-6 border-t border-white/10">
-                <dl className="divide-y divide-white/10">
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                        <dt className="text-sm/6 font-medium text-gray-100">
-                            Full name
-                        </dt>
-                        <dd className="mt-1 text-sm/6 text-gray-400 sm:col-span-2 sm:mt-0">
-                            Margot Foster
-                        </dd>
-                    </div>
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                        <dt className="text-sm/6 font-medium text-gray-100">
-                            Application for
-                        </dt>
-                        <dd className="mt-1 text-sm/6 text-gray-400 sm:col-span-2 sm:mt-0">
-                            Backend Developer
-                        </dd>
-                    </div>
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                        <dt className="text-sm/6 font-medium text-gray-100">
-                            Email address
-                        </dt>
-                        <dd className="mt-1 text-sm/6 text-gray-400 sm:col-span-2 sm:mt-0">
-                            margotfoster@example.com
-                        </dd>
-                    </div>
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                        <dt className="text-sm/6 font-medium text-gray-100">
-                            Salary expectation
-                        </dt>
-                        <dd className="mt-1 text-sm/6 text-gray-400 sm:col-span-2 sm:mt-0">
-                            $120,000
-                        </dd>
-                    </div>
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                        <dt className="text-sm/6 font-medium text-gray-100">
-                            About
-                        </dt>
-                        <dd className="mt-1 text-sm/6 text-gray-400 sm:col-span-2 sm:mt-0">
-                            Fugiat ipsum ipsum deserunt culpa aute sint do
-                            nostrud anim incididunt cillum culpa consequat.
-                            Excepteur qui ipsum aliquip consequat sint. Sit id
-                            mollit nulla mollit nostrud in ea officia proident.
-                            Irure nostrud pariatur mollit ad adipisicing
-                            reprehenderit deserunt qui eu.
-                        </dd>
-                    </div>
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                        <dt className="text-sm/6 font-medium text-gray-100">
-                            Attachments
-                        </dt>
-                        <dd className="mt-2 text-sm text-white sm:col-span-2 sm:mt-0">
-                            <ul
-                                role="list"
-                                className="divide-y divide-white/5 rounded-md border border-white/10"
-                            >
-                                <li className="flex items-center justify-between py-4 pr-5 pl-4 text-sm/6">
-                                    <div className="flex w-0 flex-1 items-center">
-                                        <PaperClipIcon
-                                            aria-hidden="true"
-                                            className="size-5 shrink-0 text-gray-500"
-                                        />
-                                        <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                                            <span className="truncate font-medium text-white">
-                                                resume_back_end_developer.pdf
-                                            </span>
-                                            <span className="shrink-0 text-gray-500">
-                                                2.4mb
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <div className="ml-4 shrink-0">
-                                        <a
-                                            href="#"
-                                            className="font-medium text-indigo-400 hover:text-indigo-300"
-                                        >
-                                            Download
-                                        </a>
-                                    </div>
-                                </li>
-                                <li className="flex items-center justify-between py-4 pr-5 pl-4 text-sm/6">
-                                    <div className="flex w-0 flex-1 items-center">
-                                        <PaperClipIcon
-                                            aria-hidden="true"
-                                            className="size-5 shrink-0 text-gray-500"
-                                        />
-                                        <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                                            <span className="truncate font-medium text-white">
-                                                coverletter_back_end_developer.pdf
-                                            </span>
-                                            <span className="shrink-0 text-gray-500">
-                                                4.5mb
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <div className="ml-4 shrink-0">
-                                        <a
-                                            href="#"
-                                            className="font-medium text-indigo-400 hover:text-indigo-300"
-                                        >
-                                            Download
-                                        </a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </dd>
-                    </div>
-                </dl>
-            </div>
+        <div className="mx-auto my-12 max-w-4xl bg-muted">
+            <Card className="p-8">
+                <div className="px-4 sm:px-0">
+                    <h3 className="text-base/7 font-semibold text-primary">
+                        Profile information
+                    </h3>
+                    <p className="mt-1 max-w-2xl text-sm/6 text-muted-foreground">
+                        Personal details.
+                    </p>
+                    <CardAction
+                        className="flex justify-end"
+                        style={{ marginTop: '-50px', marginBottom: '0px' }}
+                    >
+                        <Avatar className="w-24 h-24">
+                            <AvatarImage
+                                src={
+                                    user
+                                        ? user.photoUrl
+                                        : 'https://github.com/shadcn.png'
+                                }
+                            />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </CardAction>
+                </div>
+                <div className="mt-6 border-t border-border">
+                    <dl className="divide-y divide-border">
+                        <div className="px-4 py-6 flex items-start sm:px-0">
+                            <dt className="text-sm/6 font-medium text-foreground min-w-[150px]">
+                                Full name
+                            </dt>
+                            <dd className="ml-12 text-sm/6 text-muted-foreground">
+                                {user ? user.username : 'Margot Foster'}
+                            </dd>
+                        </div>
+                        <div className="px-4 py-6 flex items-start sm:px-0">
+                            <dt className="text-sm/6 font-medium text-foreground min-w-[150px]">
+                                Connected accounts:
+                            </dt>
+                            <dd className="ml-12 text-sm/6 text-muted-foreground">
+                                ORCID, LinkedIN, FaceBook, Google
+                            </dd>
+                        </div>
+                        <div className="px-4 py-6 flex items-start sm:px-0">
+                            <dt className="text-sm/6 font-medium text-foreground min-w-[150px]">
+                                Email address
+                            </dt>
+                            <dd className="ml-12 text-sm/6 text-muted-foreground">
+                                margotfoster@example.com
+                            </dd>
+                        </div>
+                        <div className="px-4 py-6 flex items-start sm:px-0">
+                            <dt className="text-sm/6 font-medium text-foreground min-w-[150px]">
+                                Posts
+                            </dt>
+                            <dd className="ml-12 text-sm/6 text-muted-foreground">
+                                see posts
+                            </dd>
+                        </div>
+                        <div className="px-4 py-6 flex items-start sm:px-0">
+                            <dt className="text-sm/6 font-medium text-foreground min-w-[150px]">
+                                About
+                            </dt>
+                            <dd className="ml-12 text-sm/6 text-muted-foreground">
+                                Fugiat ipsum ipsum deserunt culpa aute sint do
+                                nostrud anim incididunt cillum culpa consequat.
+                                Excepteur qui ipsum aliquip consequat sint. Sit
+                                id mollit nulla mollit nostrud in ea officia
+                                proident. Irure nostrud pariatur mollit ad
+                                adipisicing reprehenderit deserunt qui eu.
+                            </dd>
+                        </div>
+                        <div className="px-4 py-6 flex items-start sm:px-0">
+                            <dt className="text-sm/6 font-medium text-foreground min-w-[150px]">
+                                Followers
+                            </dt>
+                            <dd className="ml-12 text-sm/6 text-muted-foreground">
+                                (155) | Expand Followers
+                            </dd>
+                        </div>
+                        <div className="px-4 py-6 flex items-start sm:px-0">
+                            <dt className="text-sm/6 font-medium text-foreground min-w-[150px]">
+                                Following
+                            </dt>
+                            <dd className="ml-12 text-sm/6 text-muted-foreground">
+                                (24) | Expand Profiles
+                            </dd>
+                        </div>
+                    </dl>
+                </div>
+            </Card>
         </div>
     );
 }
