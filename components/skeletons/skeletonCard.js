ed747cbe530572d5f7137cc1ab07fcb6b6260a57
hallsarturo <proalarturo@gmail.com>
2025-08-08 09:16:33 -0600

    feat: update SkeletonCard and Skeleton components; improve layout and styling for better visual consistency

--- components/skeletons/skeletonCard.js
+++ components/skeletons/skeletonCard.js
@@ -2,11 +2,11 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export function SkeletonCard() {
   return (
-    <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+    <div className="flex flex-col justify-center gap-4 space-y-3 m-8">
+      <Skeleton className="h-[125px] w-[250px] rounded-xl mb-4" />
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-4 w-[250px] mb-4" />
+        <Skeleton className="h-4 w-[200px] mb-4" />
       </div>
     </div>
   )
