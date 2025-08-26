6f15fc34931583d2a3bdc25831477dfb031b658c
hallsarturo <proalarturo@gmail.com>
2025-08-26 12:10:12 -0600

    feat: enhance PostCardHeader layout for better responsiveness and user interaction

--- components/feed/post-card/post-card-header.js
+++ components/feed/post-card/post-card-header.js
@@ -55,82 +55,87 @@ export function PostCardHeader({
                         </div>
                     </CardDescription>
                     {/* CardAction as a row for screens < 455px */}
-                    <div className="flex gap-2 items-center justify-end flex-row min-[455px]:hidden mt-[-4px] mb-4 w-full">
+                    {/* <div className="flex gap-2 items-center justify-end flex-row min-[455px]:hidden mt-[-4px] mb-4 w-full">
                         <Badge className={badge} variant="">
                             {type}
-                        </Badge>
-                        {/* ...other icons/buttons... */}
-                    </div>
+                        </Badge> */}
+                    {/* ...other icons/buttons... */}
+                    {/* </div> */}
+
                     {/* Custom CardAction for screens >= 455px */}
-                    <div className="min-[455px]:flex hidden justify-end mt-[-6px] mb-2">
-                        <div className="flex flex-row gap-0">
-                            <div className="mr-1">
+                    <div className="flex flex-col sm:flex-row justify-end mt-[-6px] mb-2">
+                        <div className="flex flex-col sm:flex-row gap-0 sm:items-start items-center sm:justify-end justify-center w-full">
+                            <div className="mr-1 sm:mr-1 mr-0">
                                 <Badge className={badge} variant="">
                                     {type}
                                 </Badge>
                             </div>
-                            <Button variant="ghost" size="sm">
-                                <div className="cursor-pointer flex-shrink-0">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth={1.5}
-                                        stroke="currentColor"
-                                        className="size-5"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-                                        />
-                                    </svg>
-                                </div>
-                            </Button>
-                            <AlertDialog>
-                                <AlertDialogTrigger asChild>
-                                    <Button
-                                        variant="ghost"
-                                        className="cursor-pointer"
-                                        size="sm"
-                                    >
-                                        <div className=" flex-shrink-0">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                strokeWidth={1.5}
-                                                stroke="currentColor"
-                                                className="size-5"
+                            <div className="flex">
+                                <Button variant="ghost" size="sm">
+                                    <div className="cursor-pointer flex-shrink-0">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            strokeWidth={1.5}
+                                            stroke="currentColor"
+                                            className="size-5"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                                            />
+                                        </svg>
+                                    </div>
+                                </Button>
+                                <AlertDialog>
+                                    <AlertDialogTrigger asChild>
+                                        <Button
+                                            variant="ghost"
+                                            className="cursor-pointer"
+                                            size="sm"
+                                        >
+                                            <div className=" flex-shrink-0">
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    strokeWidth={1.5}
+                                                    stroke="currentColor"
+                                                    className="size-5"
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        d="M6 18 18 6M6 6l12 12"
+                                                    />
+                                                </svg>
+                                            </div>
+                                        </Button>
+                                    </AlertDialogTrigger>
+                                    <AlertDialogContent>
+                                        <AlertDialogHeader>
+                                            <AlertDialogTitle>
+                                                Delete Post. Are you sure?
+                                            </AlertDialogTitle>
+                                            <AlertDialogDescription>
+                                                This action cannot be undone.
+                                            </AlertDialogDescription>
+                                        </AlertDialogHeader>
+                                        <AlertDialogFooter>
+                                            <AlertDialogCancel>
+                                                Cancel
+                                            </AlertDialogCancel>
+                                            <AlertDialogAction
+                                                onClick={onDelete}
                                             >
-                                                <path
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    d="M6 18 18 6M6 6l12 12"
-                                                />
-                                            </svg>
-                                        </div>
-                                    </Button>
-                                </AlertDialogTrigger>
-                                <AlertDialogContent>
-                                    <AlertDialogHeader>
-                                        <AlertDialogTitle>
-                                            Delete Post. Are you sure?
-                                        </AlertDialogTitle>
-                                        <AlertDialogDescription>
-                                            This action cannot be undone.
-                                        </AlertDialogDescription>
-                                    </AlertDialogHeader>
-                                    <AlertDialogFooter>
-                                        <AlertDialogCancel>
-                                            Cancel
-                                        </AlertDialogCancel>
-                                        <AlertDialogAction onClick={onDelete}>
-                                            Continue
-                                        </AlertDialogAction>
-                                    </AlertDialogFooter>
-                                </AlertDialogContent>
-                            </AlertDialog>
+                                                Continue
+                                            </AlertDialogAction>
+                                        </AlertDialogFooter>
+                                    </AlertDialogContent>
+                                </AlertDialog>
+                            </div>
                         </div>
                     </div>
                 </div>
