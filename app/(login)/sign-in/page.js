70b4f041bbbdec241abe563cf1f595be91e3b245
hallsarturo <proalarturo@gmail.com>
2025-09-02 17:27:18 -0600

    Add favicon images and web manifest for improved site branding
    - Added favicon-16x16.png and favicon-32x32.png to enhance site identity.
    - Introduced favicon.ico for broader browser compatibility.
    - Created site.webmanifest to support Progressive Web App features and define app icons.

--- app/(login)/sign-in/page.js
+++ app/(login)/sign-in/page.js
@@ -1,19 +1,18 @@
-import { GalleryVerticalEnd } from 'lucide-react';
+import Image from 'next/image';
 import { LoginForm } from '@/components/loggin/login-form';
 
 export default function SignInPage() {
     return (
         <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-            <div className="flex w-full max-w-sm flex-col gap-6">
-                <a
-                    href="#"
-                    className="flex items-center gap-2 self-center font-medium"
-                >
-                    <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-                        <GalleryVerticalEnd className="size-4" />
-                    </div>
-                    Acme Inc.
-                </a>
+            <div className="flex justify-center w-full max-w-sm flex-col gap-6">
+                <div className="flex justify-center w-full">
+                    <Image
+                        src="/axiom_purple.png"
+                        width={75}
+                        height={50}
+                        alt="Axiom logo"
+                    />
+                </div>
                 <LoginForm />
             </div>
         </div>
--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { UserProvider } from '@/components/context/UserProfileContext';
 import Link from 'next/link';
+import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { Toaster } from 'sonner';
 
@@ -38,42 +39,74 @@ export default function RootLayout({ children }) {
 
                         <main className="flex-1">{children}</main>
 
-                        <footer className="relative z-10 flex flex-col sm:flex-row justify-center items-center min-h-20 sm:min-h-35 w-full px-4 gap-2 p-4 sm:gap-8 text-white bg-gradient-to-b from-violet-600 to-violet-900 dark:from-zinc-900 dark:to-zinc-950 text-xs">
-                            <div>
-                                <Button
-                                    className="bg-transparent text-xs px-2 py-1"
-                                    variant="ghost"
-                                    size="sm"
-                                >
-                                    <Link
-                                        href="https://arturoproal.com"
-                                        className="text-xs"
-                                    >
-                                        Arturo Proal Walls ©
-                                    </Link>
-                                </Button>
+                        <footer className="relative z-10 flex flex-col items-center justify-center min-h-20 w-full gap-4 p-4 sm:p-8 text-white bg-gradient-to-b from-violet-600 to-violet-900 dark:from-zinc-900 dark:to-zinc-950 text-xs">
+                            {/* Buttons section */}
+                            <div className="flex flex-col items-center w-full">
+                                <div className="flex flex-col sm:flex-row w-full justify-center items-center gap-4 sm:gap-20">
+                                    <div>
+                                        <Button
+                                            asChild
+                                            className="text-white dark:text-primary-foreground text-xs px-2 py-1"
+                                            variant="link"
+                                            size="sm"
+                                        >
+                                            <Link
+                                                href="https://arturoproal.com"
+                                                className="text-xs"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
+                                                Arturo Proal Walls ©
+                                            </Link>
+                                        </Button>
+                                    </div>
+                                    <div>
+                                        <Button
+                                            asChild
+                                            className="bg-transparent text-xs px-2 py-1"
+                                            variant="outline"
+                                            size="xs"
+                                        >
+                                            <Link
+                                                href="/"
+                                                className="text-xs"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
+                                                Buy me a coffee
+                                            </Link>
+                                        </Button>
+                                    </div>
+                                    <div>
+                                        <Button
+                                            asChild
+                                            className="text-white dark:text-primary-foreground text-xs px-2 py-1"
+                                            variant="link"
+                                            size="sm"
+                                        >
+                                            <Link
+                                                href="/"
+                                                className="text-xs"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
+                                                Legal
+                                            </Link>
+                                        </Button>
+                                    </div>
+                                </div>
                             </div>
-                            <div>
-                                <Button
-                                    className="bg-transparent text-xs px-2 py-1"
-                                    variant="outline"
-                                    size="xs"
-                                >
-                                    <Link href="/" className="text-xs">
-                                        Buy me a cofee
-                                    </Link>
-                                </Button>
-                            </div>
-                            <div>
-                                <Button
-                                    className="bg-transparent text-xs px-2 py-1"
-                                    variant="ghost"
-                                    size="sm"
-                                >
-                                    <Link href="/" className="text-xs">
-                                        Legal
-                                    </Link>
-                                </Button>
+
+                            {/* Logo section - now will appear below buttons */}
+                            <div className="flex justify-center items-center w-full">
+                                <Link href="/" className="text-xs">
+                                    <Image
+                                        src="/axiom_logo_white.png"
+                                        width={75}
+                                        height={50}
+                                        alt="Axiom logo"
+                                    />
+                                </Link>
                             </div>
                         </footer>
                     </ThemeProvider>
