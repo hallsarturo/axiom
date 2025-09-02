f799cf8619266704b74e629325efc2f0d34599f8
hallsarturo <proalarturo@gmail.com>
2025-09-02 10:52:54 -0600

    feat: enhance PostTypeProvider to persist postType in local storage; improve initialization logic

--- components/context/post-type-provider.js
+++ components/context/post-type-provider.js
@@ -1,11 +1,24 @@
 'use client';
 
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 
 const PostTypeContext = createContext();
 
 export function PostTypeProvider({ children }) {
-    const [postType, setPostType] = useState('papers'); // userPosts, news
+    const [postType, setPostType] = useState(() => {
+        if (typeof window !== 'undefined') {
+            return localStorage.getItem('postType') || 'papers';
+        }
+        return 'papers';
+    }); // userPosts, news
+
+    // Save to local storage
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            localStorage.setItem('postType', postType);
+        }
+    }, [postType]);
+
     return (
         <PostTypeContext.Provider value={{ postType, setPostType }}>
             {children}
